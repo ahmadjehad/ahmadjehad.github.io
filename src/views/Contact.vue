84fde3c887a7d526a382a5bba64aa53178565129
Ahmad J. Abu Yahya <ahmadj.abuyahya@gmail.com>
2021-02-05 16:37:09 +0200

    added contact links with font awesome

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,29 @@
 <template>
-  
+
+<a href="mailto:ahmadj.abuyahya@gmail.com">
+<i class="fas fa-envelope"></i>
+    ahmadj.abuyahya@gmail.com</a>
+
+<a href="tel:+962787244072">
+<i class="fas fa-mobile-alt"></i>
+    +962787244072</a>
+<a href="https://discordapp.com/users/200436061497851904">
+  <i class="fab fa-discord"></i>
+AceMeow96#5169
+</a>
+<a href="https://www.linkedin.com/in/ahmad-j-abu-yahya/">
+<i class="fab fa-linkedin"></i>
+Ahmad J. Abu Yahya
+</a>
+<a href="https://t.me/AceMeow96">
+<i class="fab fa-telegram"></i>
+@AceMeow96
+</a>
+<a href="https://github.com/ahmadjehad">
+<i class="fab fa-github"></i>
+Ahmad Jehad
+</a>
+
 </template>
 
 <script>
@@ -9,5 +33,5 @@ export default {
 </script>
 
 <style>
-
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.css');
 </style>
